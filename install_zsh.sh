e2920fc0fea9104273feaa38a60996fa251ce4f6
Liu Wei <ustclwwx@gmail.com>
2018-04-13 22:29:03 +0800

    modify install_zsh

--- install_zsh.sh
+++ install_zsh.sh
@@ -29,6 +29,5 @@ git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-z
 cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 # set theme to risto
 sed -i.bak 's/^[[:space:]]*ZSH_THEME=.*/ZSH_THEME="aphrodite"/' ~/.zshrc
-echo "source $HOME/scripts/add_path.sh" >> ~/.zshrc
 source ~/.zshrc
 
