2a5df0262f931958f55629de5f4c561d31819027
Liu Wei <ustclwwx@gmail.com>
2019-04-10 16:45:28 +0800

    update

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,28 +1,4 @@
 #!/bin/bash
-
-if ! [ -e $HOME/usr/bin/zsh ]; then
-    if ! [ -e $HOME/usr/bin ]; then
-      mkdir -p $HOME/usr/bin
-    fi
-    cd $HOME/Downloads
-    # ----------- Download zsh ---------------
-    if ! [ -e zsh.tar.gz ]
-    then
-    wget -O zsh.tar.gz https://sourceforge.net/projects/zsh/files/latest/download
-    fi
-    
-    # ----------- Install zsh ---------------
-    if ! [ -e zsh ]
-    then
-        mkdir zsh
-    fi
-    tar -xvzf zsh.tar.gz -C zsh --strip-components 1
-    cd zsh
-    ./configure --prefix=$HOME/usr/
-    make
-    make install
-fi
-
 # ----------- Install oh my zsh ---------------
 rm -rf ~/.oh-my-zsh
 git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
